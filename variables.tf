b8cbf18424433ef1bf332f7048c56abfb7dc1a05
danielhamelberg <44481340+danielhamelberg@users.noreply.github.com>
2022-10-14 23:27:23 +0100

    move contents to tfvars

--- variables.tf
+++ variables.tf
@@ -1,4 +1,16 @@
-gitlab_url = "https://gitlab.com/"
-gitlab_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-gitlab_group_name = "my_custom_group"
-gitlab_group_description = "group that holds the custom runners"
+variable "gitlab_url" {
+  description = "The URL of the GitLab instance"
+  default     = "https://gitlab.com/"
+}
+
+variable "gitlab_token" {
+  description = "The token for the GitLab instance"
+}
+
+variable "gitlab_group_name" {
+  description = "The name of the GitLab group"
+}
+
+variable "gitlab_group_description" {
+  description = "The description of the GitLab group"
+}

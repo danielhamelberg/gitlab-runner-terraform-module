225793495e7c209121d63a1cc60642a77efda1ac
danielhamelberg <44481340+danielhamelberg@users.noreply.github.com>
2022-10-14 23:25:36 +0100

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,4 @@
+gitlab_url = "https://gitlab.com/"
+gitlab_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+gitlab_group_name = "my_custom_group"
+gitlab_group_description = "group that holds the custom runners"
